b8bcab5de3c1acdeec04af55be0208ae7b72a24f
djanloo <zzibecu@gmail.com>
2024-01-02 16:07:17 +0100

    minor in tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,25 +1,73 @@
 from basal_gang.builder import SpikingNetwork
 
 def test_yaml_builder():
-    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
-
-def test_injector():
-    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
-    spikenet.populations["Albert"].add_injector(10.5, 2.1)
-    spikenet.populations["Albert"].monitorize()
-    spikenet.populations["MonaLisa"].monitorize()
-    
-    spikenet.interface.run(dt=0.1, time=50)
-    return spikenet
+    SpikingNetwork.from_yaml("spiking.yaml")
 
 def test_run():
     spikenet = SpikingNetwork.from_yaml("spiking.yaml")
     spikenet.interface.run(dt=0.1, time=10)
 
+"""
+Input/Output devices
+"""
+def test_spike_monitor():
+    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
+    spikenet.populations['Albert'].monitorize_spikes()
+
+def test_state_monitor():
+    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
+    spikenet.populations['Albert'].monitorize_states()
+
+def test_injector():
+    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
+    spikenet.populations["Albert"].add_injector(0.5, 2)
+
 if __name__=="__main__":
+    
+    import numpy as np
     import matplotlib.pyplot as plt 
 
-    evolved_spikenet = test_injector()
-    plt.plot(evolved_spikenet.populations['Albert'].get_data())
-    plt.plot(evolved_spikenet.populations['MonaLisa'].get_data())
+    spikenet = SpikingNetwork.from_yaml("spiking.yaml")
+    spikenet.populations["Albert"].add_injector(50.5, 2)
+    spikenet.populations["Albert"].monitorize_spikes()
+    spikenet.populations["Albert"].monitorize_states()
+    spikenet.populations["MonaLisa"].monitorize_spikes()
+    spikenet.populations["MonaLisa"].monitorize_states()
+
+
+    spikenet.interface.run(dt=0.01, time=10)
+
+    plt.plot(spikenet.populations['Albert'].get_data()["spikes"])
+    plt.plot(spikenet.populations['MonaLisa'].get_data()["spikes"])
+
+    states = np.array(spikenet.populations['Albert'].get_data()['states'])
+    print(states.shape)
+    plt.figure(2)
+    for i in range(5):
+        plt.plot(states[:, i, 0],marker=".")
+        plt.title("V")
+    plt.figure(3)
+    for i in range(5):
+        plt.plot(states[:, i, 1],marker=".")
+        plt.title("gsyn_exc")
+
+    plt.figure(4)
+    for i in range(5):
+        plt.plot(states[:, i, 2],marker=".")
+        plt.title("gsyn_inh")
+
+    plt.figure(5)
+    for i in range(5):
+        plt.plot(states[:, i, 3],marker=".")
+        plt.title("u")
+
+    states = np.array(spikenet.populations['MonaLisa'].get_data()['states'])
+
+    plt.figure(6)
+    for idx, feat in enumerate(["V", "gsyne", "gsyni", "u"]):
+        plt.plot(states[:, 0, idx], label=feat)
+    plt.title("MonaLisa")
+    plt.legend()
+
+
     plt.show()
